40db1a11e6de9a6c0469fb1cebf3910d420ac5ec
maixnor <46966993+maixnor@users.noreply.github.com>
2022-09-27 14:56:06 +0200

    tailwind config

--- tailwind.sh
+++ tailwind.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
+# install based on https://tailwindcss.com/blog/standalone-cli
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
 mv tailwindcss-linux-x64 tailwindcss
-./tailwindcss -i input.css -o /dist/bundle.css --minify
+
+# generate minified css using tailwind CLI
+./tailwindcss -i ./input.css -o ./dist/bundle.css --minify
