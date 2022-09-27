255059223519cf29a324c994e7079b403b685ea6
maixnor <46966993+maixnor@users.noreply.github.com>
2022-09-27 15:04:40 +0200

    automation using action

--- tailwind.sh
+++ tailwind.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+# !!!!! Careful - this is supposed to run during a github action
+
 # install based on https://tailwindcss.com/blog/standalone-cli
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
@@ -7,3 +9,6 @@ mv tailwindcss-linux-x64 tailwindcss
 
 # generate minified css using tailwind CLI
 ./tailwindcss -i ./input.css -o ./dist/bundle.css --minify
+
+# remove executable and input.css - should not be shipped
+rm tailwindcss input.css
